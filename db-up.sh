38fa6c3ba572c4edc732febf4952c1b241a57627
mrcportillo <marcossportillo@gmail.com>
2018-07-02 10:17:41 -0300

    db migration current work

--- db-up.sh
+++ db-up.sh
@@ -3,8 +3,8 @@
 docker rm $(docker ps -aq --filter name=prode-db-local-dev)
 
 docker run --name prode-db-local-dev \
-            -e POSTGRES_PASSWORD=Qwerty12 \
-            -e POSTGRES_USER=postgres \
-            -e POSTGRES_DB=prode-db-local-dev \
-            -p 5433:5432 \
+            -e POSTGRES_PASSWORD=docker \
+            -e POSTGRES_USER=docker \
+            -e POSTGRES_DB=prode_db \
+            -p 5432:5432 \
             -d postgres:9.5
